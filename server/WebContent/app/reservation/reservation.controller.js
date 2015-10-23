66fcc160329a50a94613f08d5141f9bba0e8f70f
KesavaEgenTraining <movetojunk@gmail.com>
2015-10-23 17:22:42 -0500

    refactor(module-final): change reservation template
    remove two columns for date, time and replace with datetime-local input
    type

--- server/WebContent/app/reservation/reservation.controller.js
+++ server/WebContent/app/reservation/reservation.controller.js
@@ -36,45 +36,11 @@
 
       console.log("reservationVm.reservation in create");
 
-      //Check if empty form is sumitted or if some of the values are untouched
-      if(reservationVm.reservation === null)
+      //check if form is valid i.e if we have pristine elements which might be
+      //missed by validation in HTML page
+      if(!reservationService
+          .isValidForm(reservationVm.reservation,form))
       {
-        form.first_name.$setDirty();
-        return;
-      }
-      else if(reservationVm.reservation.first_name === undefined || reservationVm.reservation.first_name === null)
-      {
-        form.first_name.$setDirty();
-        return;
-      }
-      else if(reservationVm.reservation.last_name === undefined || reservationVm.reservation.last_name === null)
-      {
-        form.last_name.$setDirty();
-        return;
-      }
-      else if(reservationVm.reservation.customerEmail === undefined || reservationVm.reservation.customerEmail === null)
-      {
-        form.customerEmail.$setDirty();
-        return;
-      }
-      else if(reservationVm.reservation.phone === undefined || reservationVm.reservation.phone === null)
-      {
-        form.phone.$setDirty();
-        return;
-      }
-      else if(reservationVm.reservation.partySize === undefined || reservationVm.reservation.partySize === null)
-      {
-        form.partySize.$setDirty();
-        return;
-      }
-      else if(reservationVm.reservation.date === undefined || reservationVm.reservation.date === null)
-      {
-        form.date.$setDirty();
-        return;
-      }
-      else if(reservationVm.reservation.rtime === undefined || reservationVm.reservation.rtime === null)
-      {
-        form.rtime.$setDirty();
         return;
       }
 
@@ -94,7 +60,9 @@
           {
 
           },function(errorMsg)
-          {});
+          {
+
+          });
 
       reservationVm.reservation = null;
       form.$setPristine();
--- server/WebContent/app/reservation/reservation.service.js
+++ server/WebContent/app/reservation/reservation.service.js
@@ -13,6 +13,7 @@
     function reservationService($q, $http) {
         var self = this;
 
+        self.isValidForm = isValidForm;
         self.createReservation = createReservation;
         self.editReservation = editReservation;
         self.getReservation = getReservation;
@@ -20,7 +21,54 @@
 
 
         //private members
-        function createReservation(reservation)
+        function isValidForm(reservation,form)
+        {
+            //Check if empty form is sumitted or if some of the values are untouched
+            if(reservation === null)
+            {
+                form.first_name.$setDirty();
+                return false;
+            }
+            else if(reservation.first_name === undefined || reservation.first_name === null)
+            {
+                form.first_name.$setDirty();
+                return false;
+            }
+            else if(reservation.last_name === undefined || reservation.last_name === null)
+            {
+                form.last_name.$setDirty();
+                return false;
+            }
+            else if(reservation.customerEmail === undefined || reservation.customerEmail === null)
+            {
+                form.customerEmail.$setDirty();
+                return false;
+            }
+            else if(reservation.phone === undefined || reservation.phone === null)
+            {
+                form.phone.$setDirty();
+                return false;
+            }
+            else if(reservation.partySize === undefined || reservation.partySize === null)
+            {
+                form.partySize.$setDirty();
+                return false;
+            }
+            else if(reservation.date === undefined || reservation.date === null)
+            {
+                form.date.$setDirty();
+                return false;
+            }
+            else if(reservation.rtime === undefined || reservation.rtime === null)
+            {
+                form.rtime.$setDirty();
+                return false;
+            }
+
+            return true;
+        }
+
+        function createReservation(reservation,form)
         {
             var defer = $q.defer();
 
